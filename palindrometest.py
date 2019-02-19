83c8f6ff1321c88f61a3a0d32f53323c7753ef7a
Alexandra <39980517+alexandradev@users.noreply.github.com>
2019-02-19 16:25:07 +0700

    Create palindrometest.py

---
+++ palindrometest.py
@@ -0,0 +1,13 @@
+word = input("Write a word:")
+a = len(word)
+error = 0
+for i in range(a//2):
+if word[i] != word[-1 - i]:
+error = 1
+break
+
+if error == 1:
+print("It's not a palindrome")
+else:
+print("It's a palindrome")
+
