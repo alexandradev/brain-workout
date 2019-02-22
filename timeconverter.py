a73315c1b7ec2b2961349563b459fd317cdf4ed1
Alexandra <39980517+alexandradev@users.noreply.github.com>
2019-02-22 12:47:42 +0700

    Update timeconverter.py
    'while' loop was added

--- timeconverter.py
+++ timeconverter.py
@@ -1,13 +1,15 @@
 
 import math
 
-number = (input('Time in minutes:'))
+while True:
+        number = (input("Write time in minutes, use 'q' for exit:"))
+        if number == 'q':
+            break
+        elif not number.isnumeric():
+            print("Error, don't use letters and signs!")
+        else:
+            hour = math.floor(int(number)/60)
+            minutes = int(number)%60
 
-if not number.isnumeric():
-    print("Error, don't use letters and signs")
-else:
-    hour = math.floor(int(number)/60)
-    minutes = int(number)%60
-
-    print('{}h {}m'.format(hour, minutes))
-    
+            print('{}h {}m'.format(hour, minutes))
+            break
