bd468b39a09ab273b020f97789d9c39142a81e7a
Alexandra <39980517+alexandradev@users.noreply.github.com>
2019-02-25 15:45:37 +0700

    Update timeconverter.py
    added more variables
    
    Thanks @FrolovIlja 😸

--- timeconverter.py
+++ timeconverter.py
@@ -1,15 +1,21 @@
-
 import math
 
 while True:
-        number = (input("Write time in minutes, use 'q' for exit:"))
-        if number == 'q':
-            break
-        elif not number.isnumeric():
-            print("Error, don't use letters and signs!")
-        else:
-            hour = math.floor(int(number)/60)
-            minutes = int(number)%60
+    number = (input("Write time in minutes, use 'q' for exit:"))
+    if number == 'q':
+        break
+    elif not number.isnumeric():
+        print("Error, don't use letters and signs!")
+    else:
+        number = int(number)
+        minutes = number % 60
+        number = math.floor(number / 60)  # here are only hours
 
-            print('{}h {}m'.format(hour, minutes))
-            break
+        hours = math.floor(number % 24)
+        number = math.floor(number / 24)  # here are only days
+
+        days = math.floor(number % 30)
+        number = math.floor(number / 30)  # here are only months
+
+        print('{} months {} days {} hours {} minutes'.format(number, days, hours, minutes))
+        break
